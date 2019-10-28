8fd73088b76a6b0357a3d7ec1023f9f0437c967f
ye.renming <ye.renming@immomo.com>
2019-10-29 00:42:32 +0800

    1

---
+++ Morty/Morty/Script/generate_reflector.py
@@ -0,0 +1,27 @@
+#coding:UTF-8
+import sys, getopt, os, time
+
+
+def record_reflector(headerFile):
+    print(headerFile)
+
+
+def main(argv):
+
+    if len(argv) < 1:
+        return
+
+    path = argv[0]
+    filterSuffix = [".h"]
+
+    for mainDir, _, fileNames in os.walk(path):
+
+        for fileName in fileNames:
+            fullPath = os.path.join(mainDir, fileName)
+            suffix = os.path.splitext(fullPath)[1]
+
+            if suffix in filterSuffix:
+                record_reflector(fullPath)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
