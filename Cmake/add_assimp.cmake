b0051317ab625be1bac8dcf195f26347b4ba8692
DoubleYe <654065929@qq.com>
2023-03-01 00:46:38 +0800

    add vcpkg

--- Cmake/add_assimp.cmake
+++
@@ -1,6 +0,0 @@
-﻿
-############################################################
-# assimp
-############################################################
-set(assimp_DIR ${THIRD_PARTY_PATH}/installs/assimp/lib/cmake/assimp-5.0)
-find_package(assimp)
