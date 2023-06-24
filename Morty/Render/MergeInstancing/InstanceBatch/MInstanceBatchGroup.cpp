b8e07cc80e7401cfa7866306491381e016e8e5d6
DoubleYe <654065929@qq.com>
2023-06-25 01:17:02 +0800

    refactor:提交文件删除记录

--- Morty/Render/MergeInstancing/InstanceBatch/MInstanceBatchGroup.cpp
+++
@@ -1,9 +0,0 @@
-#include "MInstanceBatchGroup.h"
-
-void MInstanceBatchGroup::UpdateVisible(MMeshInstanceKey key, bool bVisible)
-{
-    if (auto pInstance = FindMeshInstance(key))
-    {
-        pInstance->bVisible = bVisible;
-    }
-}
